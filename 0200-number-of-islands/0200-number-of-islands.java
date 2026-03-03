507dfb3bee7d0ba6586f0038fff96b93f4b3b69c
Anusha-0913 <vtu26642@veltech.edu.in>
2026-03-03 10:26:56 +0530

    Time: 3 ms (86.86%), Space: 52.6 MB (32.86%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int count = 0;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j, m, n);
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    private void dfs(char[][] grid, int i, int j, int m, int n) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') return;
+        
+        grid[i][j] = '0'; 
+        
+        dfs(grid, i + 1, j, m, n);
+        dfs(grid, i - 1, j, m, n);
+        dfs(grid, i, j + 1, m, n);
+        dfs(grid, i, j - 1, m, n);
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        char[][] grid1 = {
+            {'1','1','1','1','0'},
+            {'1','1','0','1','0'},
+            {'1','1','0','0','0'},
+            {'0','0','0','0','0'}
+        };
+        System.out.println(sol.numIslands(grid1)); 
+
+        char[][] grid2 = {
+            {'1','1','0','0','0'},
+            {'1','1','0','0','0'},
+            {'0','0','1','0','0'},
+            {'0','0','0','1','1'}
+        };
+        System.out.println(sol.numIslands(grid2)); 
+    }
+}
