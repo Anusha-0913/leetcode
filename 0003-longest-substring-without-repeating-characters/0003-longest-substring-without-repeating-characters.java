cfe12befcff9081d7c9141a2447bc8b34fd6264a
Anusha-0913 <vtu26642@veltech.edu.in>
2026-03-04 09:06:10 +0530

    Time: 5 ms (86.43%), Space: 45.2 MB (86.41%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -6,18 +6,18 @@ class Solution {
 
         HashMap<Character, Integer> map = new HashMap<>();
         int maxLen = 0;
-        int start = 0; // start of the current window
+        int start = 0; 
 
         for (int end = 0; end < s.length(); end++) {
             char c = s.charAt(end);
 
-            // If character is already in the window, move start
+            
             if (map.containsKey(c)) {
-                // Move start to the next of last seen index of c
+                
                 start = Math.max(map.get(c) + 1, start);
             }
 
-            map.put(c, end); // Update last seen index
+            map.put(c, end); 
             maxLen = Math.max(maxLen, end - start + 1);
         }
 
