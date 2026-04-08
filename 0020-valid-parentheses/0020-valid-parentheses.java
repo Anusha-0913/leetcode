c6a52271651cff0760c0626250c589a32833391f
Anusha-0913 <vtu26642@veltech.edu.in>
2026-04-08 09:33:01 +0530

    Time: 2 ms (97.89%), Space: 43.4 MB (47.27%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,15 +1,20 @@
-import java.util.*;
+import java.util.Stack;
+
 class Solution {
-public boolean isValid(String s) {
-Deque<Character> stack = new ArrayDeque<>();
-for (char c : s.toCharArray()) {
-if (c == '(') stack.push(')');
-else if (c == '[') stack.push(']');
-else if (c == '{') stack.push('}');
-else {
-if (stack.isEmpty() || stack.pop() != c) return false;
-}
-}
-return stack.isEmpty();
-}
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(')');
+            else if (c == '{') stack.push('}');
+            else if (c == '[') stack.push(']');
+            else {
+                if (stack.isEmpty() || stack.pop() != c) {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
 }
